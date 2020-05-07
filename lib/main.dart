4ff780b60af93162cb97487f8442c0b2d2042615
Sérgio Ildefonso <sergio_ildefonso@hotmail.com>
2020-05-07 01:09:40 +0100

    Estável e com bloqueio de orientação vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:math';
 
 void main() => runApp(HitCrocApp());
 
-class HitCrocApp extends StatefulWidget {
-  @override
-  _HitCrocAppState createState() => _HitCrocAppState();
-}
-
-class _HitCrocAppState extends State<HitCrocApp> {
-  double left = 10;
-  double right = 10;
-  double top = 10;
-  double bottom = 10;
-
+class HitCrocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text('Hit the Croc'),
-        ),
-        body: SafeArea(
-          child: Container(
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(left, top, right, bottom),
-                child: FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      left = Random().nextDouble() * 300;
-                      right = Random().nextDouble() * 300;
-                      top = Random().nextDouble() * 300;
-                      bottom = Random().nextDouble() * 300;
-                      print('left $left');
-                      print('right $right');
-                      print('top $top');
-                      print('bottom $bottom');
-                    });
-                  },
-                  child: Image.asset(
-                    'assets/croc.png',
-                    width: 100,
-                    height: 100,
-                  ),
-                )),
+      home: AppBody(),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  double left = 1;
+  double right = 1;
+  double top = 1;
+  double bottom = 1;
+  int pointsA = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Hit the Croc'),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  double _width = MediaQuery.of(context).size.width;
+                  double _height = MediaQuery.of(context).size.height;
+                  double _tmpleft = Random().nextDouble() * _width - 100;
+                  double _tmptop = Random().nextDouble() * _height - 150;
+                  left = _tmpleft > 0 ? _tmpleft : 0;
+                  top = _tmptop > 0 ? _tmptop : 0;
+                  pointsA = pointsA + 1;
+                });
+              },
+              child: Image.asset(
+                'assets/croc.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
           ),
+        ),
+      ),
+      backgroundColor: Colors.lightGreen.shade400,
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Pontos: $pointsA'),
+            SizedBox(
+              width: 30,
+            ),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  pointsA = 0;
+                });
+              },
+              child: Text('Recomeçar'),
+            ),
+            SizedBox(
+              width: 30,
+            ),            
+            SizedBox(
+              width: 30,
+            ),
+          ],
         ),
       ),
     );
